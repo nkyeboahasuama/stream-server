fe87eec5378b50ec84fc958337ba159d33ebe339
Nana <ynkwame@gmail.com>
2024-02-29 22:53:55 -0600

    Server: readable stream

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
 const cors = require("cors");
 
 const app = express();
 const port = 3005;
 app.use(cors());
+
 app.get("/stream-file", (req, res) => {
   const filePath = "./writeFile.txt";
-  const fileStream = fs.createReadStream(filePath, { highWaterMark: 20 });
+  const fileStream = fs.createReadStream(filePath, { highWaterMark: 16000 });
 
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
 
   fileStream.on("data", (chunk) => {
-    fileStream.pause();
-    paused = true;
-
-    setTimeout(() => {
-      res.write(`data: ${chunk.toString("utf-8")}\n\n`);
-      paused = false;
-      fileStream.resume();
-    }, 100);
+    res.write(`data: ${chunk.toString("utf-8")}\n\n`);
   });
 
   fileStream.on("end", () => {
+    console.log("end");
     res.end();
+  });
+  fileStream.on("error", () => {
+    console.log("Error in file");
+    res.status(500).send("Server error");
   });
 });
 
